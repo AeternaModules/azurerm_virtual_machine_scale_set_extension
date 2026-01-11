b3787de06c0f9d51608c7ac85d2839753a2df03e
Daniel Allen <dan@aeternagroup.net>
2026-01-11 22:19:50 +0000

    Update to schema version v4.57.0

---
+++ outputs.tf
@@ -0,0 +1,5 @@
+output "virtual_machine_scale_set_extensions" {
+  description = "All virtual_machine_scale_set_extension resources"
+  value       = azurerm_virtual_machine_scale_set_extension.virtual_machine_scale_set_extensions
+}
+
